d68d612fb12b02934198f0dab15b8dec76727e7e
super_link <752577456@qq.com>
2019-04-29 16:07:15 +0800

    修改了个人信息

---
+++ app/src/main/java/com/vivwe/personal/activity/MyFansActivity.java
@@ -0,0 +1,21 @@
+package com.vivwe.personal.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.vivwe.base.activity.BaseActivity;
+import com.vivwe.main.R;
+
+/**
+ * ahtor: super_link
+ * date: 2019/4/26 17:59
+ * remark: 我的关注
+ */
+public class MyFansActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_myfans);
+    }
+}
