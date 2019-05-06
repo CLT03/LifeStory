689e060df35783fefed3f46b29888d3fc9a62889
super_link <752577456@qq.com>
2019-05-06 16:01:37 +0800

    添加图片

---
+++ app/src/main/java/com/vivwe/main/activity/ResetPasswordActivity.java
@@ -0,0 +1,21 @@
+package com.vivwe.main.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.vivwe.base.activity.BaseActivity;
+import com.vivwe.main.R;
+
+/**
+ * ahtor: super_link
+ * date: 2019/4/26 18:12
+ * remark: 关于生活故事
+ */
+public class ResetPasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset);
+    }
+}
