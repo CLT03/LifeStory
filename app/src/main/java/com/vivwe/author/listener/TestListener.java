750527b3a060e5074ec6337437edca665884ce66
super_link <752577456@qq.com>
2019-05-08 11:03:55 +0800

    ddd

---
+++ app/src/main/java/com/vivwe/author/listener/TestListener.java
@@ -0,0 +1,9 @@
+package com.vivwe.author.listener;
+
+/**
+ * ahtor: super_link
+ * date: 2019/5/8 11:01
+ * remark:
+ */
+public class TestListener {
+}
---
+++ app/src/main/java/com/vivwe/personal/adapter/TestAdapter.java
@@ -0,0 +1,9 @@
+package com.vivwe.personal.adapter;
+
+/**
+ * ahtor: super_link
+ * date: 2019/5/8 11:01
+ * remark:
+ */
+public class TestAdapter {
+}
