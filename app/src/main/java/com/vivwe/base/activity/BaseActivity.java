b277f83feeb1f70da668cdc3512a205da26e6d10
super_link <752577456@qq.com>
2019-05-09 16:46:39 +0800

    搞定了，可以下班了

--- app/src/main/java/com/vivwe/base/activity/BaseActivity.java
+++ app/src/main/java/com/vivwe/base/activity/BaseActivity.java
@@ -17,6 +17,8 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+//        setTheme(R.style.AppImmersiveTheme);
     }
 
 
