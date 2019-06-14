2dcd98d64f065787d0d61472c4d5184d8a57b8fa
super_link <752577456@qq.com>
2019-06-14 10:01:43 +0800

    冲突

--- app/src/main/java/com/vivwe/base/ui/cardstack/DefaultStackEventListener.kt
+++ app/src/main/java/com/vivwe/base/ui/cardstack/DefaultStackEventListener.kt
@@ -17,50 +17,27 @@ class DefaultStackEventListener(i:Int) : CardStack.CardEventListener {
 
     override fun swipeEnd(section: Int, distance: Float): Boolean {
 
-<<<<<<< HEAD
-        //Log.d("rae", "swipeEnd:$section-$distance")
-=======
-        Log.d("rae", "swipeEnd:$section-$distance")
->>>>>>> 6c7b0e53cb6702995fb197eb2be3181452c27ad1
 
         return distance > mThreshold
     }
 
     override fun swipeStart(section: Int, distance: Float): Boolean {
 
-<<<<<<< HEAD
-        //Log.d("rae", "swipeStart:$section-$distance")
-=======
-        Log.d("rae", "swipeStart:$section-$distance")
->>>>>>> 6c7b0e53cb6702995fb197eb2be3181452c27ad1
         return false
     }
 
     override fun swipeContinue(section: Int, distanceX: Float, distanceY: Float): Boolean {
 
-<<<<<<< HEAD
-       // Log.d("rae", "swipeContinue:$section-$distanceX-$distanceY")
-=======
-        Log.d("rae", "swipeContinue:$section-$distanceX-$distanceY")
->>>>>>> 6c7b0e53cb6702995fb197eb2be3181452c27ad1
 
         return false
     }
 
     override fun discarded(mIndex: Int, direction: Int) {
 
-<<<<<<< HEAD
        // Log.d("rae", "discarded:$mIndex-$direction")
     }
 
     override fun topCardTapped() {
         //Log.d("rae", "topCardTapped")
-=======
-        Log.d("rae", "discarded:$mIndex-$direction")
-    }
-
-    override fun topCardTapped() {
-        Log.d("rae", "topCardTapped")
->>>>>>> 6c7b0e53cb6702995fb197eb2be3181452c27ad1
     }
 }
