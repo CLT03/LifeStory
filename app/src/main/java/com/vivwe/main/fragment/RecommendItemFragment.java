ab2ffb2cb37a905ca03d4babc7f551e109626359
哈哈王 <cltplh@163.com>
2019-06-18 15:55:21 +0800

    下拉刷新

--- app/src/main/java/com/vivwe/main/fragment/RecommendItemFragment.java
+++ app/src/main/java/com/vivwe/main/fragment/RecommendItemFragment.java
@@ -12,6 +12,10 @@ import android.view.ViewGroup;
 import com.mbs.sdk.net.HttpRequest;
 import com.mbs.sdk.net.listener.OnResultListener;
 import com.mbs.sdk.net.msg.WebMsg;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.vivwe.base.activity.BaseFragment;
 import com.vivwe.base.ui.alert.Toast;
 import com.vivwe.main.R;
@@ -29,7 +33,12 @@ public class RecommendItemFragment extends BaseFragment {
     RecyclerView recyclerView;
     Unbinder unbinder;
     RecommendItemAdapter adapter;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
     private VideoEntity videoEntity;
+    private int pageNum=1;
+    private int pageSize=20;
+    private int mRefreshOrLoadMore;//1是刷新 2是加载更多
 
     @Nullable
     @Override
@@ -40,30 +49,56 @@ public class RecommendItemFragment extends BaseFragment {
         return view;
     }
 
-    private void init(){
+    private void init() {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
         recyclerView.setLayoutManager(gridLayoutManager);
         adapter = new RecommendItemAdapter(getActivity());
         recyclerView.setAdapter(adapter);
-        getData();
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                pageNum=1;
+                mRefreshOrLoadMore=1;
+                getData();
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                if(videoEntity.getMyVideoList().size()==pageSize) {
+                    pageNum++;
+                    mRefreshOrLoadMore=2;
+                    getData();
+                }else {
+                    android.widget.Toast.makeText(getContext(), "没有更多数据~", Toast.LENGTH_SHORT).show();
+                    refreshLayout.finishLoadMore();
+                }
+            }
+        });
+        if (videoEntity != null) {
+            adapter.setVideos(videoEntity.getMyVideoList());
+        }else getData();
     }
 
+
     private void getData(){
-        if(videoEntity!=null){
-            adapter.setVideos(videoEntity.getMyVideoList());
-        }else if (getArguments() != null) {
-            HttpRequest.getInstance().excute(HttpRequest.create(VideoApi.class).getVideoByType(1,Integer.MAX_VALUE,getArguments().getInt("tag")), new OnResultListener() {
-                @Override
-                public void onWebUiResult(WebMsg webMsg) {
-                    if (webMsg.dataIsSuccessed()) {
-                        videoEntity = webMsg.getData(VideoEntity.class);
-                        adapter.setVideos(videoEntity.getMyVideoList());
-                    } else if (webMsg.netIsSuccessed()) {
-                        Toast.show(getContext(), webMsg.getDesc(), 2000);
-                    }
+        HttpRequest.getInstance().excute(HttpRequest.create(VideoApi.class).getVideoByType(pageNum,pageSize, getArguments().getInt("tag")), new OnResultListener() {
+            @Override
+            public void onWebUiResult(WebMsg webMsg) {
+                if (webMsg.dataIsSuccessed()) {
+                    videoEntity = webMsg.getData(VideoEntity.class);
+                    adapter.setVideos(videoEntity.getMyVideoList());
+                    if(mRefreshOrLoadMore==1){
+                        refreshLayout.finishRefresh();
+                        android.widget.Toast.makeText(getContext(), "刷新成功！", Toast.LENGTH_SHORT).show();
+                    }else if(mRefreshOrLoadMore==2)
+                        refreshLayout.finishLoadMore();
+
+                } else if (webMsg.netIsSuccessed()) {
+                    Toast.show(getContext(), webMsg.getDesc(), 2000);
                 }
-            });
-        }
+            }
+        });
     }
 
 
